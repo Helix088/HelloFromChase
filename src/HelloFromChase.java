fe255fb88ee26467a21550e69f696dde7f00a0fa
Helix088 <pat20001@byui.edu>
2021-10-15 18:48:54 -0600

    Fixed code so it would run

--- src/HelloFromChase.java
+++
@@ -1,5 +0,0 @@
-public class HelloFromChase {
-    System.out.println("Hello, my name is Chase!\n");
-    System.out.println("Hello Chase, my name is Paul!\n");
-    System.out.println("Hello Chase, my name is Kara!\n");
-}
--- src/patterson/HelloFromChase.java
+++ src/patterson/HelloFromChase.java
@@ -2,7 +2,7 @@ package patterson;
 
 public class HelloFromChase {
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Chase!");
+        System.out.println("Hello, my name is Chase!\n");
         System.out.println("Hello Chase, my name is Paul!\n");
         System.out.println("Hello Chase, my name is Kara!\n");
     }
