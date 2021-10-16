6453889945471ae6d74ded8b1747e7b9bf8c84e1
Chase Patterson <76974505+Helix088@users.noreply.github.com>
2021-10-15 18:47:14 -0600

    Fixed the code so it would run

--- src/HelloFromChase.java
+++ src/HelloFromChase.java
@@ -1,5 +1,9 @@
+package patterson;
+
 public class HelloFromChase {
-    System.out.println("Hello, my name is Chase!\n");
-    System.out.println("Hello Chase, my name is Paul!\n");
-    System.out.println("Hello Chase, my name is Kara!\n");
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Chase!\n");
+        System.out.println("Hello Chase, my name is Paul!\n");
+        System.out.println("Hello Chase, my name is Kara!\n");
+    }
 }
